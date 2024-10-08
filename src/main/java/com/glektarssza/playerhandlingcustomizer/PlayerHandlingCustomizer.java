b1aca4b503d19894dcdf0456f041844937b553b5
G'lek Tarssza <glek@glektarssza.com>
2024-10-08 13:37:49 -0600

    Corrected some (obvious now) errors.

--- src/main/java/com/glektarssza/playerhandlingcustomizer/PlayerHandlingCustomizer.java
+++ src/main/java/com/glektarssza/playerhandlingcustomizer/PlayerHandlingCustomizer.java
@@ -1,40 +1,49 @@
 package com.glektarssza.playerhandlingcustomizer;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
  * The root mod class.
  */
-@Mod(PlayerHandlingCustomizer.MOD_ID)
+@Mod(modid = PlayerHandlingCustomizer.MOD_ID, name = PlayerHandlingCustomizer.MOD_NAME, version = PlayerHandlingCustomizer.MOD_VERSION, dependencies = PlayerHandlingCustomizer.MOD_DEPENDENCIES)
 public class PlayerHandlingCustomizer {
     /**
      * The ID of the mod.
      */
-    public static final String MOD_ID = "playerhandlingcustomizer";
+    public static final String MOD_ID = "${mod_id}";
+
+    /**
+     * The human-readable name of the mod.
+     */
+    public static final String MOD_NAME = "${mod_name}";
+
+    /**
+     * The human-readable version of the mod.
+     */
+    public static final String MOD_VERSION = "${mod_version}";
 
     /**
      * The logger to use for the mod.
      */
-    public static final Logger LOGGER = LogManager.getLogger();
+    public static Logger LOGGER;
 
     /**
-     * Create a new instance.
+     * The mod singleton instance.
      */
-    public PlayerHandlingCustomizer() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::Init);
-    }
+    @Mod.Instance
+    public static PlayerHandlingCustomizer instance;
 
     /**
      * Initialize the mod.
      *
      * @param event The event to handle.
      */
-    public void Init(FMLCommonSetupEvent event) {
+    @Mod.EventHandler
+    public void Init(FMLPreInitializationEvent event) {
+        LOGGER = event.getModLog();
         LOGGER.info("Initializing PlayerHandlingCustomizer...");
         // TODO
     }
