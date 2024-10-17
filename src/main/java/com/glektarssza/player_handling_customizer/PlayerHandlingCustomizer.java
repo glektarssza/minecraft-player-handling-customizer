8d819d7c8eeb22dec145da11b162e0fafa9f8d9d
G'lek Tarssza <glek@glektarssza.com>
2024-10-17 07:31:02 -0600

    Added some config-related event handling.

--- src/main/java/com/glektarssza/player_handling_customizer/PlayerHandlingCustomizer.java
+++ src/main/java/com/glektarssza/player_handling_customizer/PlayerHandlingCustomizer.java
@@ -2,8 +2,14 @@ package com.glektarssza.player_handling_customizer;
 
 import org.apache.logging.log4j.Logger;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Config.Type;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
  * The root mod class.
@@ -27,10 +33,34 @@ public class PlayerHandlingCustomizer {
      * @param event The event to handle.
      */
     @Mod.EventHandler
-    public void OnPreInit(FMLPreInitializationEvent event) {
+    public void onPreInit(FMLPreInitializationEvent event) {
         LOGGER = event.getModLog();
         LOGGER.info("Pre-initializing {}...", Tags.MOD_NAME);
-        // TODO: Pre-initialization
+        MinecraftForge.EVENT_BUS.register(this);
         LOGGER.info("Done pre-initializing {}!", Tags.MOD_NAME);
+    }
+
+    /**
+     * Handle the Forge Mod Loader pre-initialization event.
+     *
+     * @param event The event to handle.
+     */
+    @Mod.EventHandler
+    public void onInit(FMLInitializationEvent event) {
+        LOGGER.info("Initializing {}...", Tags.MOD_NAME);
+        LOGGER.info("Synchronizing confiugration for {}...", Tags.MOD_NAME);
+        ConfigManager.sync(Tags.MOD_ID, Type.INSTANCE);
+        LOGGER.info("Done Initializing {}!", Tags.MOD_NAME);
+    }
+
+    /**
+     * An event handler for when a configuration changes.
+     *
+     * @param event The event data.
+     */
+    @SubscribeEvent
+    public void onConfigChange(OnConfigChangedEvent event) {
+        LOGGER.info("Synchronizing confiugration for {}...", Tags.MOD_NAME);
+        ConfigManager.sync(Tags.MOD_ID, Type.INSTANCE);
     }
 }
