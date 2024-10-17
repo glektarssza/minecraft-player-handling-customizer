ec37c9e96c2c9e8e6993075d4862a33d65a5a858
G'lek Tarssza <glek@glektarssza.com>
2024-10-17 07:11:13 -0600

    Working on immunity stuff.

---
+++ src/main/java/com/glektarssza/player_handling_customizer/mixins/vanilla/EntityAITargetMixin.java
@@ -0,0 +1,38 @@
+package com.glektarssza.player_handling_customizer.mixins.vanilla;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.glektarssza.player_handling_customizer.api.ITargetingImmunity;
+import com.glektarssza.player_handling_customizer.utils.ImmunityUtils;
+import com.glektarssza.player_handling_customizer.utils.PlayerUtils;
+
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.EntityAITarget;
+import net.minecraft.entity.player.EntityPlayer;
+
+@Mixin(EntityAITarget.class)
+public class EntityAITargetMixin {
+    @Inject(method = "Lnet/minecraft/entity/ai/EntityAITarget;isSuitableTarget(Lnet/minecraft/entity/EntityLiving;Lnet/minecraft/entity/EntityLivingBase;ZZ)Z", at = @At("TAIL"))
+    private static void isSuitableTarget(EntityLiving attacker,
+        @Nullable EntityLivingBase target, boolean includeInvincibles,
+        boolean checkSight, CallbackInfoReturnable<Boolean> cir) {
+        if (!(target instanceof EntityPlayer)) {
+            return;
+        }
+        EntityPlayer player = (EntityPlayer) target;
+        List<ITargetingImmunity> immunities = PlayerUtils
+            .getPlayerTargetingImmunities(player);
+        if (ImmunityUtils.entityMatchesAnyTargetingImmunity(attacker,
+            immunities)) {
+            cir.setReturnValue(false);
+        }
+    }
+}
