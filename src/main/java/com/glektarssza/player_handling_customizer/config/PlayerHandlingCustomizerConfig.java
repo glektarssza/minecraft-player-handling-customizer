fedba3f3110aebbc1578b0907a15e42aca0abe78
G'lek Tarssza <glek@glektarssza.com>
2024-10-17 08:47:22 -0600

    Translate config entry.

--- src/main/java/com/glektarssza/player_handling_customizer/config/PlayerHandlingCustomizerConfig.java
+++ src/main/java/com/glektarssza/player_handling_customizer/config/PlayerHandlingCustomizerConfig.java
@@ -4,6 +4,7 @@ import com.glektarssza.player_handling_customizer.Tags;
 
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.Config.Comment;
+import net.minecraftforge.common.config.Config.LangKey;
 import net.minecraftforge.common.config.Config.Type;
 
 /**
@@ -14,6 +15,7 @@ public class PlayerHandlingCustomizerConfig {
     /**
      * A list of players, by UUID of username, who are immune to being targeted.
      */
+    @LangKey("player_handling_customizer.config.immune_players")
     @Comment({ "A list of players who are immune to being targeted.",
         "This can be either the username or UUID of the player." })
     public static String[] immunePlayers = {};
