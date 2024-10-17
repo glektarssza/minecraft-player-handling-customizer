dc4ff80c39e5859d150ec02f47abc4a447de6987
G'lek Tarssza <glek@glektarssza.com>
2024-10-17 07:15:39 -0600

    Added a temporary config.

---
+++ src/main/java/com/glektarssza/player_handling_customizer/config/PlayerHandlingCustomizerConfig.java
@@ -0,0 +1,20 @@
+package com.glektarssza.player_handling_customizer.config;
+
+import com.glektarssza.player_handling_customizer.Tags;
+
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.Comment;
+import net.minecraftforge.common.config.Config.Type;
+
+/**
+ * The main configuration for the mod.
+ */
+@Config(modid = Tags.MOD_ID, name = Tags.MOD_ID, type = Type.INSTANCE)
+public class PlayerHandlingCustomizerConfig {
+    /**
+     * A list of players, by UUID of username, who are immune to being targeted.
+     */
+    @Comment({ "A list of players who are immune to being targeted.",
+        "This can be either the username or UUID of the player." })
+    public static String[] immunePlayers = {};
+}
