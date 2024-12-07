b2bf3c357b83c24146b0c6d75c0ac26395cc1c17
G'lek Tarssza <glek@glektarssza.com>
2024-12-06 20:20:32 -0700

    Remove unused import.

--- src/main/java/com/glektarssza/player_handling_customizer/mixins/divinerpg/AIDivineFireballAttackMixin.java
+++ src/main/java/com/glektarssza/player_handling_customizer/mixins/divinerpg/AIDivineFireballAttackMixin.java
@@ -2,7 +2,6 @@ package com.glektarssza.player_handling_customizer.mixins.divinerpg;
 
 import java.util.List;
 
-import org.omg.CORBA.Object;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
