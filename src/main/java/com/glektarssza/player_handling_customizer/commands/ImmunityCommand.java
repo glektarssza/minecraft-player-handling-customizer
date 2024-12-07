69be2f00af5487f219fafcd1e56a4bc707430cc0
G'lek Tarssza <glek@glektarssza.com>
2024-12-06 20:20:38 -0700

    Bit of code cleanup.

--- src/main/java/com/glektarssza/player_handling_customizer/commands/ImmunityCommand.java
+++ src/main/java/com/glektarssza/player_handling_customizer/commands/ImmunityCommand.java
@@ -5,6 +5,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import com.glektarssza.player_handling_customizer.api.IImmunity;
 import com.glektarssza.player_handling_customizer.api.IPhysicalImmunity;
 import com.glektarssza.player_handling_customizer.api.ImmunityType;
@@ -32,34 +35,34 @@ public class ImmunityCommand extends CommandBase {
     }
 
     @Override
-    public String getUsage(ICommandSender sender) {
+    public String getUsage(@Nonnull ICommandSender sender) {
         return String.format(
-            "/%s <player> <add|remove|list> [*|damage|hurt|knockback|targeting] [*|entityType] [*|direct|indirect]",
-            this.getName());
+                "/%s <player> <add|remove|list> [*|damage|hurt|knockback|targeting] [*|entityType] [*|direct|indirect]",
+                this.getName());
     }
 
     @Override
-    public boolean isUsernameIndex(String[] args, int index) {
+    public boolean isUsernameIndex(@Nonnull String[] args, int index) {
         return index == 1;
     }
 
     @Override
-    public List<String> getTabCompletions(MinecraftServer server,
-        ICommandSender sender, String[] args, BlockPos targetPos) {
+    public List<String> getTabCompletions(@Nonnull MinecraftServer server,
+            @Nonnull ICommandSender sender, @Nonnull String[] args, @Nullable BlockPos targetPos) {
         if (args.length == 1) {
             return Arrays.asList(server.getOnlinePlayerNames());
         } else if (args.length == 2) {
             return Arrays.asList("add", "remove", "list");
         } else if (args.length == 3) {
             return Arrays.asList("*", "damage", "hurt", "knockback",
-                "targeting");
+                    "targeting");
         } else if (args.length == 4) {
             List<String> allOptions = EntityList.getEntityNameList().stream()
-                .map((rl) -> rl.toString()).collect(Collectors.toList());
+                    .map((rl) -> rl.toString()).collect(Collectors.toList());
             allOptions.add(0, "*");
             return allOptions;
         } else if (args.length == 5 && (args[3].equalsIgnoreCase("damage")
-            || args[3].equalsIgnoreCase("hurt"))) {
+                || args[3].equalsIgnoreCase("hurt"))) {
             return Arrays.asList("*", "direct", "indirect");
         } else {
             return Collections.emptyList();
@@ -67,23 +70,24 @@ public class ImmunityCommand extends CommandBase {
     }
 
     @Override
-    public void execute(MinecraftServer server, ICommandSender sender,
-        String[] args) throws CommandException {
+    public void execute(@Nonnull MinecraftServer server,
+            @Nonnull ICommandSender sender,
+            @Nonnull String[] args) throws CommandException {
         if (args.length < 2) {
             throw new SyntaxErrorException(
-                "player_handling_customizer.commands.errors.syntax.missing_player_name");
+                    "player_handling_customizer.commands.errors.syntax.missing_player_name");
         }
         if (args.length < 3) {
             throw new SyntaxErrorException(
-                "player_handling_customizer.commands.errors.syntax.missing_operation");
+                    "player_handling_customizer.commands.errors.syntax.missing_operation");
         }
         String playerName = args[1];
         EntityPlayerMP player = server.getPlayerList()
-            .getPlayerByUsername(playerName);
+                .getPlayerByUsername(playerName);
         if (player == null) {
             throw new PlayerNotFoundException(
-                "player_handling_customizer.commands.errors.player_not_found",
-                playerName);
+                    "player_handling_customizer.commands.errors.player_not_found",
+                    playerName);
         }
         String operation = args[2];
         String immunityType = "*";
@@ -100,10 +104,10 @@ public class ImmunityCommand extends CommandBase {
         }
         if (operation.equalsIgnoreCase("add")) {
             throw new CommandException(
-                "player_handling_customizer.commands.errors.work_in_progress");
+                    "player_handling_customizer.commands.errors.work_in_progress");
         } else if (operation.equalsIgnoreCase("remove")) {
             throw new CommandException(
-                "player_handling_customizer.commands.errors.work_in_progress");
+                    "player_handling_customizer.commands.errors.work_in_progress");
         } else if (operation.equalsIgnoreCase("list")) {
             List<? extends IImmunity> immunities = null;
             if (immunityType.equalsIgnoreCase("*")) {
@@ -118,31 +122,31 @@ public class ImmunityCommand extends CommandBase {
                 immunities = PlayerUtils.getPlayerTargetingImmunities(player);
             } else {
                 throw new SyntaxErrorException(
-                    "player_handling_customizer.commands.errors.syntax.invalid_immunity_type",
-                    immunityType);
+                        "player_handling_customizer.commands.errors.syntax.invalid_immunity_type",
+                        immunityType);
             }
             sender.sendMessage(new TextComponentTranslation(
-                "player_handling_customizer.commands.response.list.header",
-                playerName));
+                    "player_handling_customizer.commands.response.list.header",
+                    playerName));
             immunities.forEach((immunity) -> {
                 ImmunityType type = immunity.getImmunityType();
                 ITextComponent immunityTypeTranslation = null;
                 switch (type) {
                     case Damage:
                         if (((IPhysicalImmunity) immunity)
-                            .getAppliesToDirectDamage()
-                            && ((IPhysicalImmunity) immunity)
+                                .getAppliesToDirectDamage()
+                                && ((IPhysicalImmunity) immunity)
+                                        .getAppliesToIndirectDamage()) {
+                            immunityTypeTranslation = new TextComponentTranslation(
+                                    "player_handling_customizer.immunity.damage.upper");
+                        } else if (((IPhysicalImmunity) immunity)
+                                .getAppliesToDirectDamage()) {
+                            immunityTypeTranslation = new TextComponentTranslation(
+                                    "player_handling_customizer.immunity.damage.lower");
+                        } else if (((IPhysicalImmunity) immunity)
                                 .getAppliesToIndirectDamage()) {
                             immunityTypeTranslation = new TextComponentTranslation(
-                                "player_handling_customizer.immunity.damage.upper");
-                        } else if (((IPhysicalImmunity) immunity)
-                            .getAppliesToDirectDamage()) {
-                            immunityTypeTranslation = new TextComponentTranslation(
-                                "player_handling_customizer.immunity.damage.lower");
-                        } else if (((IPhysicalImmunity) immunity)
-                            .getAppliesToIndirectDamage()) {
-                            immunityTypeTranslation = new TextComponentTranslation(
-                                "player_handling_customizer.immunity.damage.lower");
+                                    "player_handling_customizer.immunity.damage.lower");
                         } else {
                             // How did we get here?????
                             return;
@@ -150,19 +154,19 @@ public class ImmunityCommand extends CommandBase {
                         break;
                     case Hurt:
                         if (((IPhysicalImmunity) immunity)
-                            .getAppliesToDirectDamage()
-                            && ((IPhysicalImmunity) immunity)
+                                .getAppliesToDirectDamage()
+                                && ((IPhysicalImmunity) immunity)
+                                        .getAppliesToIndirectDamage()) {
+                            immunityTypeTranslation = new TextComponentTranslation(
+                                    "player_handling_customizer.immunity.hurt.upper");
+                        } else if (((IPhysicalImmunity) immunity)
+                                .getAppliesToDirectDamage()) {
+                            immunityTypeTranslation = new TextComponentTranslation(
+                                    "player_handling_customizer.immunity.hurt.lower");
+                        } else if (((IPhysicalImmunity) immunity)
                                 .getAppliesToIndirectDamage()) {
                             immunityTypeTranslation = new TextComponentTranslation(
-                                "player_handling_customizer.immunity.hurt.upper");
-                        } else if (((IPhysicalImmunity) immunity)
-                            .getAppliesToDirectDamage()) {
-                            immunityTypeTranslation = new TextComponentTranslation(
-                                "player_handling_customizer.immunity.hurt.lower");
-                        } else if (((IPhysicalImmunity) immunity)
-                            .getAppliesToIndirectDamage()) {
-                            immunityTypeTranslation = new TextComponentTranslation(
-                                "player_handling_customizer.immunity.hurt.lower");
+                                    "player_handling_customizer.immunity.hurt.lower");
                         } else {
                             // How did we get here?????
                             return;
@@ -170,11 +174,11 @@ public class ImmunityCommand extends CommandBase {
                         break;
                     case Knockback:
                         immunityTypeTranslation = new TextComponentTranslation(
-                            "player_handling_customizer.immunity.knockback.upper");
+                                "player_handling_customizer.immunity.knockback.upper");
                         break;
                     case Targeting:
                         immunityTypeTranslation = new TextComponentTranslation(
-                            "player_handling_customizer.immunity.targeting.upper");
+                                "player_handling_customizer.immunity.targeting.upper");
                         break;
                 }
             });
@@ -182,8 +186,8 @@ public class ImmunityCommand extends CommandBase {
 
         {
             throw new SyntaxErrorException(
-                "player_handling_customizer.commands.errors.syntax.invalid_operation",
-                operation);
+                    "player_handling_customizer.commands.errors.syntax.invalid_operation",
+                    operation);
         }
     }
 }
