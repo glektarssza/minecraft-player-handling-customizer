c2643f80879a65692ab3b7ca0a6a20f80aaf71ac
G'lek Tarssza <glek@glektarssza.com>
2025-04-29 15:26:04 -0600

    Fixed bad mixin.

--- src/main/java/com/glektarssza/player_handling_customizer/mixins/early/vanilla/EntityAITargetMixin.java
+++ src/main/java/com/glektarssza/player_handling_customizer/mixins/early/vanilla/EntityAITargetMixin.java
@@ -2,12 +2,14 @@ package com.glektarssza.player_handling_customizer.mixins.early.vanilla;
 
 import java.util.List;
 
+import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAITarget;
 import net.minecraft.entity.player.EntityPlayer;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -22,14 +24,21 @@ import com.glektarssza.player_handling_customizer.utils.PlayerUtils;
 @Mixin(EntityAITarget.class)
 public class EntityAITargetMixin {
     /**
+     * A shadow of the {@code taskOwner} field.
+     */
+    @Shadow
+    private EntityCreature taskOwner;
+
+    /**
      * Mixin for the {@code isSuitableTarget} method.
      */
     @SuppressWarnings("unused")
     @Inject(method = "isSuitableTarget", at = @At("TAIL"), cancellable = true)
-    private void isSuitableTarget(EntityLiving attacker,
-        EntityLivingBase target, boolean includeInvincibles,
+    private void isSuitableTarget(EntityLivingBase target,
+        boolean includeInvincibles,
         CallbackInfoReturnable<Boolean> cir) {
         EntityAITarget self = (EntityAITarget) (Object) this;
+        EntityLiving attacker = taskOwner;
         if (!(target instanceof EntityPlayer)) {
             return;
         }
