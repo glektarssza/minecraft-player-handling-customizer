8f02df8758963d20be962e69e373a8786455ac33
G'lek Tarssza <glek@glektarssza.com>
2025-04-29 20:52:16 -0600

    Remove unused imports.

--- src/main/java/com/glektarssza/player_handling_customizer/mixins/late/specialmobs/Entity_SpecialGhastMixin.java
+++ src/main/java/com/glektarssza/player_handling_customizer/mixins/late/specialmobs/Entity_SpecialGhastMixin.java
@@ -2,13 +2,11 @@ package com.glektarssza.player_handling_customizer.mixins.late.specialmobs;
 
 import java.util.List;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
