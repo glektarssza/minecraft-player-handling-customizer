2027354925fbf3ca269b248667d0bf58b1abadd2
G'lek Tarssza <glek@glektarssza.com>
2025-04-29 20:52:34 -0600

    Change how special spiders are managed.

--- src/main/java/com/glektarssza/player_handling_customizer/mixins/late/specialmobs/Entity_SpecialSpiderMixin.java
+++ src/main/java/com/glektarssza/player_handling_customizer/mixins/late/specialmobs/Entity_SpecialSpiderMixin.java
@@ -1,12 +1,20 @@
 package com.glektarssza.player_handling_customizer.mixins.late.specialmobs;
 
+import java.util.List;
+
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.glektarssza.player_handling_customizer.api.ITargetingImmunity;
+import com.glektarssza.player_handling_customizer.utils.ImmunityUtils;
+import com.glektarssza.player_handling_customizer.utils.PlayerUtils;
 
 import toast.specialMobs.entity.spider.Entity_SpecialSpider;
 
@@ -18,9 +26,27 @@ public class Entity_SpecialSpiderMixin {
     /**
      * Mixin for the {@code findPlayerToAttack} method.
      */
-    @Inject(method = "findPlayerToAttack", at = @At(value = "INVOKE", target = "net.minecraft.entity.monster.EntitySpider.findPlayerToAttack()Lnet/minecraft/entity/Entity;"), cancellable = false, remap = false)
+    @Inject(method = "findPlayerToAttack", at = @At("TAIL"), cancellable = true, remap = false)
     public void findPlayerToAttack(CallbackInfoReturnable<Entity> cir) {
         Entity_SpecialSpider self = (Entity_SpecialSpider) (Object) this;
-        self.fakeDarkness = false;
+        EntityLiving attacker = self;
+        Entity returnValue = cir.getReturnValue();
+        if (returnValue == null) {
+            return;
+        }
+        if (!(returnValue instanceof EntityLivingBase)) {
+            return;
+        }
+        EntityLivingBase target = (EntityLivingBase) returnValue;
+        if (!(target instanceof EntityPlayer)) {
+            return;
+        }
+        EntityPlayer player = (EntityPlayer) target;
+        List<ITargetingImmunity> immunities = PlayerUtils
+            .getPlayerTargetingImmunities(player);
+        if (ImmunityUtils.entityMatchesAnyTargetingImmunity(attacker,
+            immunities) || PlayerUtils.getIsPlayerGloballyImmune(player)) {
+            cir.setReturnValue(null);
+        }
     }
 }
