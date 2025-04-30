312e542519a385e0b333d93019abb2ff69ffa322
G'lek Tarssza <glek@glektarssza.com>
2025-04-30 10:59:59 -0600

    Inject into `EntityCreature` to override setting target during `updateEntityActionState` calls.

---
+++ src/main/java/com/glektarssza/player_handling_customizer/mixins/early/vanilla/EntityCreatureMixin.java
@@ -0,0 +1,53 @@
+package com.glektarssza.player_handling_customizer.mixins.early.vanilla;
+
+import java.util.List;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.glektarssza.player_handling_customizer.api.ITargetingImmunity;
+import com.glektarssza.player_handling_customizer.utils.ImmunityUtils;
+import com.glektarssza.player_handling_customizer.utils.PlayerUtils;
+
+/**
+ * Mixin for the {@code EntityCreature} class.
+ */
+@Mixin(EntityCreature.class)
+public class EntityCreatureMixin {
+    /**
+     * A shadow of the {@code entityToAttack} field.
+     */
+    @Shadow
+    private Entity entityToAttack;
+
+    /**
+     * Mixin for the {@code updateEntityActionState} method.
+     */
+    @Inject(method = "updateEntityActionState", at = @At(value = "INVOKE_ASSIGN", target = "net.minecraft.entity.EntityCreature.findPlayerToAttack()Lnet/minecraft/entity/Entity;"))
+    public void overrideTargetedEntity(CallbackInfo ci) {
+        EntityCreature self = (EntityCreature) (Object) this;
+        EntityLiving attacker = self;
+        Entity target = this.entityToAttack;
+        if (target == null) {
+            return;
+        }
+        if (!(target instanceof EntityPlayer)) {
+            return;
+        }
+        EntityPlayer player = (EntityPlayer) target;
+        List<ITargetingImmunity> immunities = PlayerUtils
+            .getPlayerTargetingImmunities(player);
+        if (ImmunityUtils.entityMatchesAnyTargetingImmunity(attacker,
+            immunities) || PlayerUtils.getIsPlayerGloballyImmune(player)) {
+            this.entityToAttack = null;
+        }
+    }
+}
