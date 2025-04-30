ae938786a9a27fd419d28afd5d6a91f72982c46d
G'lek Tarssza <glek@glektarssza.com>
2025-04-29 21:33:35 -0600

    Prevent retaliation when hurting mobs.

--- src/main/java/com/glektarssza/player_handling_customizer/mixins/early/vanilla/EntityMobMixin.java
+++ src/main/java/com/glektarssza/player_handling_customizer/mixins/early/vanilla/EntityMobMixin.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.DamageSource;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -41,6 +43,34 @@ public class EntityMobMixin {
         if (ImmunityUtils.entityMatchesAnyTargetingImmunity(attacker,
             immunities) || PlayerUtils.getIsPlayerGloballyImmune(player)) {
             cir.setReturnValue(null);
+        }
+    }
+
+    /**
+     * Mixin for the {@code attackEntityFrom} method.
+     */
+    @Inject(method = "attackEntityFrom", at = @At("TAIL"), cancellable = true)
+    public void attackEntityFrom(DamageSource damageSource, float amount,
+        CallbackInfoReturnable<Boolean> cir) {
+        EntityMob self = (EntityMob) (Object) this;
+        EntityLiving attacker = self;
+        Entity targetEntity = self.getEntityToAttack();
+        if (targetEntity == null) {
+            return;
+        }
+        if (!(targetEntity instanceof EntityLivingBase)) {
+            return;
+        }
+        EntityLivingBase target = (EntityLivingBase) targetEntity;
+        if (!(target instanceof EntityPlayer)) {
+            return;
+        }
+        EntityPlayer player = (EntityPlayer) target;
+        List<ITargetingImmunity> immunities = PlayerUtils
+            .getPlayerTargetingImmunities(player);
+        if (ImmunityUtils.entityMatchesAnyTargetingImmunity(attacker,
+            immunities) || PlayerUtils.getIsPlayerGloballyImmune(player)) {
+            self.setTarget(null);
         }
     }
 }
